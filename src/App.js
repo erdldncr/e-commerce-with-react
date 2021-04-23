538a0826ffca9eee0b37f04717cd4cd75ec484b7
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-04-23 13:15:04 +0100

    react router  added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import {Switch,Route} from 'react-router-dom'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
+import Details from './components/Details'
+import Cart from './components/Cart'
+import Default from './components/Default'
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <h3>App Component</h3>
+        <Navbar/>
+        <Switch>
+          <Route path='/' exact component={ProductList}/>
+          <Route path='/details'  component={Details} />
+          <Route path='/cart'  component={Cart} />
+          <Route path='*' component={Default} />
+        </Switch>
       </React.Fragment>
     );
   }
---
+++ src/components/Cart.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Card extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello Card Comp</h3>
+            </div>
+        )
+    }
+}
