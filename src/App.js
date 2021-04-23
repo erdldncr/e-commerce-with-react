9cb362a6e19f31ed841942a5f8f293d21d59355b
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-04-23 20:04:10 +0100

    product component and its styles have been added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import ProductList from './components/ProductList'
 import Details from './components/Details'
 import Cart from './components/Cart'
 import Default from './components/Default'
-
 class App extends Component {
   render() {
     return (
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react'
-
+import styled from 'styled-components'
+import {Link} from'react-router-dom'
+import {ProductConsumer} from '../Context'
 export default class Product extends Component {
     render() {
+        const{company,count,id,img,inCart,info,price,title,total}=this.props
         return (
-            <div>
-                Product Component
-            </div>
+            <ProductWrapper className='col-9 mx-auto col-md-6 col-lg-3 my-3'>
+                <div className="card">
+                    <div className="img-container p-5" onClick={()=>console.log('clicked me')}>
+                    <Link to={`/details/`} >
+                    <img src={img} alt={company} className='card-img-top' />
+                    </Link>
+                    <button onClick={()=>console.log('button onclick')} className="card-btn" disabled={inCart?true:false}>
+                        {inCart? 
+                        (<p className="text-capitalize mb-0" disabled>In Cart</p>):
+                         <i className="fas fa-cart-plus"/>   }
+                    </button>
+                    </div>
+                    {/* card-footer */}
+                    <div className="card-footer d-flex justify-content-between">
+                        <p className="align-self-center mb-0">
+                        {title}
+                        </p>
+                        <h5 className="text-blue font-italic mb-0">
+                            <span className="mr-1">${price}</span>
+                        </h5>
+                    </div>
+                </div>
+            </ProductWrapper>
         )
     }
 }
+
+const ProductWrapper=styled.div`
+.card{
+    border-color:transparent;
+    transition:all 1s linear;
+}
+.card-footer{
+    background:transparent;
+    border-top:transparent;
+}
+&:hover{
+    .card{
+        border:0.04rem solid rgba(0,0,0,0.2);
+        box-shadow:2px 2px 5px 0px rgba(0,0,0,0.2);
+    }
+    .card-footer{
+        background:rgba(247,247,247);
+    }
+}
+`
